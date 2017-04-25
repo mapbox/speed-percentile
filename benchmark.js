8ac38b565c878a6d7d0b7d99e3434f9721157a47
lily-chai <lily@mapbox.com>
2017-04-25 00:06:50 -0700

    remove R4, add inverse, fix R5

--- benchmark.js
+++ benchmark.js
@@ -1,19 +1,21 @@
-var buffer = require('./');
+var PercentileInterpolator = require('./lib/percentile');
 var Benchmark = require('benchmark');
 var fs = require('fs');
 var path = require('path');
 
-var algorithms = ['R4', 'R5'];
-var p = 0.7;
 var infile = path.join(__dirname, '/test/simulate_100000/normal');
 var testData = JSON.parse(fs.readFileSync(infile));
+var pi = new PercentileInterpolator(testData);
+
 
 var suite = new Benchmark.Suite('speed-percentile');
 
-algorithms.forEach(function(flag) {
-  suite.add('speed-percentile#' + flag, function() {
-    buffer(testData, p, flag);
-  });
+suite.add('speed-percentile#getSpeed', function() {
+  pi.getSpeed(0.7);
+});
+
+suite.add('speed-percentile#getPercentile', function() {
+  pi.getPercentile(45);
 });
 
 suite.on('cycle', function(event) {
